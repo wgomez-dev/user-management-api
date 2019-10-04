a502a3bbb3f244a04ac9dc7b48565950c6314f2c
wgomez-dev <wilson.gomez.consultant@sothebys.com>
2019-10-04 10:34:45 -0500

    setup basic crud for users

--- users.js
+++ users.js
@@ -2,11 +2,12 @@ const express = require('express');
 const bodyparse = require('body-parser');
 var admin = require("firebase-admin");
 
+var user = require('./models/user');
+
 const app = express();
 
 app.use(bodyparse.urlencoded({ extended: false }));
 app.use(bodyparse.json());
-
 
 // Fetch the service account key JSON file contents
 var serviceAccount = require("./user-managment-55d82-firebase-adminsdk-w16vr-a037b389fb");
@@ -22,14 +23,40 @@ var db = admin.database();
 
 
 app.get('/', (req, res) => {
-    res.send('Main endpoint');
+    res.send('User Managment API entry point');
 })
 
-app.post('/guardar',(req, res) => {
+app.get('/users', (req, res) => {
+    db.ref("users/").once('value', function(snapshot){
+        res.send(snapshot.val());
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    db.ref("users/"+req.params.id).once('value', function(snapshot){
+        res.send(snapshot.val());
+    })
+})
+
+app.post('/users',(req, res) => {
     console.log(req.body);
-    db.ref("users").push(req.body);
-    res.send("Recibido");
+    db.ref("users/"+req.body.id).set(req.body);
+    res.send("Saved");
 });
+
+app.delete('/users/:id', (req, res) => {
+    db.ref("users/"+req.params.id).remove().then(function() {
+        res.send("Remove succeeded.")
+      })
+      .catch(function(error) {
+        res.send("Remove failed: " + error.message)
+      });
+})
+
+app.put('/users/:id', (req, res) => {
+    db.ref("users/"+req.params.id).set(req.body);
+    res.send("Updated");
+})
 
 app.listen(3000, () => {
     console.log('Up and running! -- user managment application')
