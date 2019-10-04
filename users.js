37299a2c363f259d6fb2cc96f74c96f52dee1fea
wgomez-dev <wilson.gomez.consultant@sothebys.com>
2019-10-04 02:16:44 -0500

    set up basic post with firebase

--- users.js
+++ users.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const bodyparse = require('body-parser');
+var admin = require("firebase-admin");
+
 const app = express();
+
+app.use(bodyparse.urlencoded({ extended: false }));
+app.use(bodyparse.json());
+
+
+// Fetch the service account key JSON file contents
+var serviceAccount = require("./user-managment-55d82-firebase-adminsdk-w16vr-a037b389fb");
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://user-managment-55d82.firebaseio.com/"
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+
 
 app.get('/', (req, res) => {
     res.send('Main endpoint');
 })
+
+app.post('/guardar',(req, res) => {
+    console.log(req.body);
+    db.ref("users").push(req.body);
+    res.send("Recibido");
+});
 
 app.listen(3000, () => {
     console.log('Up and running! -- user managment application')
